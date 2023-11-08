c21023e7feba8998d7e22ef2f56d04eb0748c641
liujian <liujian@hudiexiaoying.cn>
2023-11-08 20:29:25 +0800

    update blog config

--- themes/gitbook/components/ArticleInfo.js
+++ themes/gitbook/components/ArticleInfo.js
@@ -4,6 +4,7 @@ export default function ArticleInfo({ post }) {
   }
   return <div className="pt-10 pb-6 text-gray-400 text-sm border-b">
         <i className="fa-regular fa-clock mr-1" />
-        Last update:  { post.date?.start_date}
+          {/* Last update:  { post.date?.start_date} */}
+        { post.date?.start_date}
     </div>
 }
