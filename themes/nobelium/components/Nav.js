5f4547028d00e342962c8f824bbe92b1695d495b
liujian <liujian@hudiexiaoying.cn>
2024-02-21 20:51:49 +0800

    Navbar click event

--- themes/nobelium/components/Nav.js
+++ themes/nobelium/components/Nav.js
@@ -56,15 +56,15 @@ const Nav = props => {
                 </Link>
                 {navBarTitle
                   ? (
-                        <p className="ml-2 font-medium text-gray-800 dark:text-gray-300 header-name">
+                        <Link href="/" className="ml-2 font-medium text-gray-800 dark:text-gray-300 header-name">
                             {navBarTitle}
-                        </p>
+                        </Link>
                     )
                   : (
-                        <p className="ml-2 font-medium text-gray-800 dark:text-gray-300 header-name whitespace-nowrap">
+                        <Link href="/" className="ml-2 font-medium text-gray-800 dark:text-gray-300 header-name whitespace-nowrap">
                             {siteConfig('TITLE')}
                             {/* ,{' '}<span className="font-normal">{siteConfig('DESCRIPTION')}</span> */}
-                        </p>
+                        </Link>
                     )}
             </div>
             <NavBar {...props} />
